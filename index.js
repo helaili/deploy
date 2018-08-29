02f0afa5f0fc3f86817969f5bdfad7150f72dd3d
Alain Hélaïli <helaili@github.com>
2018-08-29 09:04:32 +0200

    remove comment & log

--- index.js
+++ index.js
@@ -3,9 +3,8 @@
  * @param {import('probot').Application} app - Probot's Application class.
  */
 module.exports = app => {
-  // Your code here
   app.log('Yay, the app was loaded!')
-
+  
   const getConfig = require('probot-config')
 
   // Get an express router to expose new HTTP endpoints
@@ -29,8 +28,6 @@ module.exports = app => {
     let encodedLabelName = encodeURI(labelName)
 
     if (config.labels[encodedLabelName]) {
-      context.log.debug(config.labels[encodedLabelName], 'Label config found')
-
       let deployment = config.labels[encodedLabelName]
       deployment.owner = context.payload.pull_request.head.repo.owner.login
       deployment.repo = context.payload.pull_request.head.repo.name
